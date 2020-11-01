00266d7a8cdf88177dc8bdcdfe86d665fe06b557
ChenZhixia-stu <1760405308@qq.com>
2020-11-01 16:51:20 +0800

    完成注册和登录功能

---
+++ src/main/java/project/subs/dao/UserDao.java
@@ -0,0 +1,8 @@
+package project.subs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.subs.bean.User;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+     User findUserByUsername(String username);
+}
---
+++ src/main/java/project/subs/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package project.subs.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.subs.bean.User;
+import project.subs.dao.UserDao;
+import project.subs.service.IUserService;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User findUserByUsername(String username) {
+        return userDao.findUserByUsername(username);
+    }
+
+    /**
+     * 注册用户
+     *
+     * @param user
+     */
+    @Override
+    public void saveUser(User user) {
+        System.out.println("service层接收数据：" + user);
+        userDao.save(user);
+    }
+
+    @Override
+    public User verify(String username) {
+        User user = userDao.findUserByUsername(username);
+        System.out.println(user);
+        return user;
+    }
+
+}
